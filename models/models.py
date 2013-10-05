67bf6d61a391592eb56c4dd6abd5de34e5c7c826
mattmattmattwhatwhat <matthew@gm1ca-74c.local>
2013-10-05 13:04:43 -0400

    more lift models

--- models/models.py
+++ models/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 class LiftData(models.Model):
     liftName = models.CharField(max_length=200)
     liftMax = models.IntegerField(default=0)
     userId = models.ForeignKey(User)
+
+class LiftHistory(models.Model):
+    liftData = LiftData()
+    liftDate = models.DateField.auto_now_add()
+    week = models.IntegerField(default=0)
+    cycle = models.IntegerField(default=0)
+
+class LiftSet(models.Model):
+    rep = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
