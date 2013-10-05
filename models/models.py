8a2383ef0f048a997ad0653824e4c2f982308d96
Woy Vien <woyvien@gmail.com>
2013-10-05 13:27:19 -0400

    conflicts

--- models/models.py
+++ models/models.py
@@ -3,22 +3,16 @@ from django.contrib.auth.models import User
 from datetime import date
 
 class LiftData(models.Model):
-<<<<<<< HEAD
-    liftName = models.CharField(max_length=200)
-    liftMax = models.IntegerField(default=0)
-    userId = models.ForeignKey(User)
+    name = models.CharField(max_length=200)
+    one_rep_max = models.IntegerField(default=0)
+    user = models.ForeignKey(User)
 
 class LiftHistory(models.Model):
     liftData = LiftData()
-    liftDate = models.DateField.auto_now_add()
+    liftDate = models.DateField(date.today)
     week = models.IntegerField(default=0)
     cycle = models.IntegerField(default=0)
 
 class LiftSet(models.Model):
     rep = models.IntegerField(default=0)
     weight = models.IntegerField(default=0)
-=======
-    name = models.CharField(max_length=200)
-    one_rep_max = models.IntegerField(default=0)
-    user = models.ForeignKey(User)
->>>>>>> rest service for liftdata
