6dfe231ee79eea460d35281b2edd40b65e7bfcf4
Woy Vien <woyvien@gmail.com>
2013-10-05 17:43:20 -0400

    search filter example

--- managers/LiftDataManager.py
+++ managers/LiftDataManager.py
@@ -41,5 +41,36 @@ class LiftDataManager():
     
     @staticmethod
     def searchLiftData(requestinUserId, searchFilter):
-        print(searchFilter.set_search_key);
+        # Generate filter string
+        filterString = ""
+        searchKey = searchFilter.get_search_key()
+        if searchKey is not None:
+            filterString += ".filter(note__icontains=searchKey)"
+#            filterString += ".filter(Q(headline__icontains=searchKey) | Q(note_description__icontains=searchKey))"
+        
+        noteTypes = searchFilter.get_note_types()
+        if noteTypes:
+            filterString += ".filter(note_type__in=noteTypes)"
+        
+        start_date = searchFilter.get_start_date()
+        if start_date:
+            filterString += ".filter(calendar_entries__start_date__gte=start_date)"
+            
+        end_date = searchFilter.get_end_date()
+        if end_date:
+            filterString += ".filter(calendar_entries__end_date__lte=end_date)"
+        
+        filterList = BLManager.getCommonCoreFilterList(searchFilter)
+        if filterList:
+            filterString += ".extra(where=filterList)"
+            
+        # Paging and Sorting
+        offset = int(searchFilter.get_offset())
+        toRow = offset + int(searchFilter.get_rowcount())
+        sort = searchFilter.get_sort()
+        # Evaluate and return values
+        
+        noteQueryString = "BLNote.objects" + filterString + ".distinct().order_by(*sort)[offset:toRow]"
+        notes = eval(noteQueryString)
+        return notes
     
